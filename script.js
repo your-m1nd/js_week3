fa4ffcb1861f6579ed7b5633c8c06ec7eba99391
your-m1nd <vi.neurogirl@gmail.com>
2023-05-20 23:10:04 +0300

    wirking on mistakes

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let priceSneakers = document.getElementById('priceSneakers');
 let priceJeans = document.getElementById('priceJeans');
 let priceBelt = document.getElementById('priceBelt');
 
-console.log(document.getElementById('priceJacket').innerHTML);
-console.log(document.getElementById('priceSneakers').innerHTML);
-console.log(document.getElementById('priceJeans').innerHTML);
-console.log(document.getElementById('priceBelt').innerHTML);
+//console.log(document.getElementById('priceJacket').innerHTML);
+//console.log(document.getElementById('priceSneakers').innerHTML);
+//console.log(document.getElementById('priceJeans').innerHTML);
+//console.log(document.getElementById('priceBelt').innerHTML);
 
 priceJacket = +"9999";
 priceSneakers = +"6390";
@@ -15,14 +15,14 @@ priceBelt = +"2550";
 
 let total = document.getElementById('total');
 total.innerHTML = `${+priceJacket+priceSneakers+priceJeans+priceBelt} руб.`;
-console.log(total.innerHTML);
+//console.log(total.innerHTML);
 
 
 let discount = 20;
 let button = document.getElementById('btn');
 button.onclick = function newPrice() {
-    total.innerHTML = `${+priceJacket+priceSneakers+priceJeans+priceBelt/100*20} руб.`;
-    console.log(total.innerHTML);
+    total.innerHTML = `${+priceJacket+priceSneakers+priceJeans+priceBelt/100*discount} руб.`;
+    //console.log(total.innerHTML);
 };
 
 let formElement = document.querySelector('.basket');
